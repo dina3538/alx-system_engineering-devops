34c326c57cdc258cad1c52cf7761bac7977262c4
dina3538 <dinaaltayeb1234@gmail.com>
2024-02-27 16:19:26 -0800

    a

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,19 @@
 #!/usr/bin/env ruby
-## A regular expression that is simply matching School
-#puts ARGV[0].scan(/School/).join
+def match_school(argument)
+  regex = /\bSchool\b/  # Regular expression to match the word "School" with word boundaries
+  if argument =~ regex
+    puts "The argument '#{argument}' contains the word 'School'."
+  else
+    puts "The argument '#{argument}' does not contain the word 'School'."
+  end
+end
+
+argument = ARGV[0]
+
+
+if argument.nil?
+  puts "Please provide an argument."
+else
+  match_school(argument)
+end
+
